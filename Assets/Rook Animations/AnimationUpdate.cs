3781c80c0a6387326f8b4427b28a9547db5fb6f8
DMGTUD <d23124992@mytudublin.ie>
2025-02-18 13:49:51 +0000

    More Animations

---
+++ Assets/Rook Animations/AnimationUpdate.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AnimationUpdate : MonoBehaviour
+{
+    public GameObject upload;
+    public float glitch;
+    public bool scan;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        glitch=upload.GetComponent<PressedKeep>().fill/upload.GetComponent<PressedKeep>().full;
+        GetComponent<Animator>().SetLayerWeight(1,glitch);
+        GetComponent<Animator>().SetFloat("Glitch",1);
+
+
+    }
+
+    public void Activate()
+    {
+        GetComponent<Animator>().SetBool("Wake",true);
+    }
+}
